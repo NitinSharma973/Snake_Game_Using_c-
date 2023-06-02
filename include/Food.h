8632d1e525651a7cb0460cc80a2e1af08981ebb8
ns7036678 <you@example.com>
2023-06-02 22:11:30 +0530

    first commit

---
+++ include/Food.h
@@ -0,0 +1,29 @@
+#ifndef FOOD_H
+#define FOOD_H
+
+#include<windows.h>
+#define WIDTH 50
+#define HEIGHT 25
+
+
+class Food
+{
+
+
+    private:
+        COORD pos;
+
+
+    public :
+
+
+
+        void gen_food();
+        COORD get_pos();
+
+
+
+
+};
+
+#endif // FOOD_H
---
+++ include/Snake.h
@@ -0,0 +1,50 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+#include <windows.h>
+
+#include<vector>
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+class Snake
+{
+
+
+    private:
+
+        COORD pos;
+
+        int len;
+        int vel;
+        char direction;
+
+        vector<COORD> body;
+
+
+
+
+    public:
+
+        Snake(COORD pos,int vel);
+
+        void change_dir(char dir);
+
+        void move_snake();
+
+  vector<COORD> get_body();
+
+        COORD get_pos();
+        bool eaten(COORD food_pos);
+
+  void grow();
+
+  bool collided();
+
+
+
+
+};
+
+#endif // SNAKE_H
